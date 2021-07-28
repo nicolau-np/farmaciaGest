009c8ca4deff171a9efe779853574d20ad242cbc
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 23:58:40 +0100

    criado o metodo para a pagina principal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'title' => "farmaciaGest",
+            'menu' => "Home",
+            'submenu' => null,
+            'type' => "home",
+            'config' => null,
+        ];
+        return view('home', $data);
+    }
 }
