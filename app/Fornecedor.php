0eb49dcd2bfad9fcf0228d1b2e1fa9d6407ef052
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 22:52:23 +0100

    trabalhando no modelo fornecedor

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Fornecedor extends Model
 {
-    //
+    protected $table = "fornecedors";
+
+    protected $fillable = [
+        'nome',
+        'email',
+        'telefone',
+        'endereco',
+        'estado',
+    ];
+
+    public function produto(){
+        return $this->hasMany(Produto::class, 'id_fornecedor', 'id');
+    }
 }
