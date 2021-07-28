a57f55e263bd51111c5ce86678db14ae80317d0b
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 23:04:09 +0100

    trabalhando no modelo de vendas

--- app/Venda.php
+++ app/Venda.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Venda extends Model
 {
-    //
+    protected $table = "vendas";
+
+    protected $fillable = [
+        'id_funcionario',
+        'id_cliente',
+        'valor_total',
+        'estado',
+    ];
+
+    public function cliente(){
+        return $this->belongsTo(Cliente::class, 'id_cliente', 'id');
+    }
+
+    public function funcionario(){
+        return $this->belongsTo(Funcionario::class, 'id_funcionario', 'id');
+    }
 }
