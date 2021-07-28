2707559b0217bbb96566372d80ca9a29274c3741
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 22:59:55 +0100

    trabalhando no modelo de clientes

--- app/Cliente.php
+++ app/Cliente.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $table = "clientes";
+
+    protected $fillable = [
+        'id_pessoa',
+        'estado',
+    ];
+
+    public function cliente(){
+        return $this->belongsTo(Pessoa::class, 'id_pessoa', 'id');
+    }
+
+    public function venda(){
+        return $this->hasMany(Venda::class, 'id_cliente', 'id');
+    }
 }
