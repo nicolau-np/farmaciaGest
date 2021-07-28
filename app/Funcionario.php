d901d39c688bd08ebf4a0700e778a5946b50ba82
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 22:38:44 +0100

    trabalhando no modelo de funcionarios

--- app/Funcionario.php
+++ app/Funcionario.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
-    
+    protected $table = "funcionarios";
+
+    protected $fillable = [
+        'id_pessoa',
+        'cargo',
+        'estado',
+    ];
+
+    public function pessoa(){
+        return $this->belongsTo(Funcionario::class, 'id_pessoa', 'id');
+    }
 }
