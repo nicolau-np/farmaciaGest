010954746a66b8661c0075f157dbca12edd5fccf
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 12:21:32 +0100

    trabalhando no modelo de pessoa

--- app/Pessoa.php
+++ app/Pessoa.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pessoa extends Model
 {
-    //
+    protected $table = "pessoas";
+
+    protected $fillable = [
+        'nome',
+        'data_nascimento',
+        'bairro',
+        'genero',
+        'telefone',
+        'email',
+        'foto',
+    ];
+
+    public function funcionario(){
+        return $this->hasMany(Funcionario::class, 'id_funcionario', 'id');
+    }
+
+    public function usuario(){
+        return $this->hasMany(User::class, 'id_pessoa', 'id');
+    }
+
+    public function cliente(){
+        return $this->hasMany(Cliente::class, 'id_pessoa', 'id');
+    }
 }
