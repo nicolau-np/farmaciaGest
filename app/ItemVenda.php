a76b0fd2c3b1c1c9a25ab2250bcb6fffa42c1714
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 23:08:03 +0100

    trabalhando no modelo de item de venda

--- app/ItemVenda.php
+++ app/ItemVenda.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class ItemVenda extends Model
 {
-    //
+    protected $table = "item_vendas";
+
+    protected $fillable = [
+        'id_venda',
+        'id_produto',
+        'quantidade',
+        'preco_unitario',
+        'estado',
+    ];
+
+    public function venda(){
+        return $this->belongsTo(Venda::class, 'id_venda', 'id');
+    }
+
+    public function produto(){
+        return $this->belongsTo(Produto::class, 'id_produto', 'id');
+    }
 }
