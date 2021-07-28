129147336315180bb5a5866d92a37990b25a8181
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-28 22:55:58 +0100

    trabalhando no modelo de fornecedor

--- app/Produto.php
+++ app/Produto.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    protected $table = "produtos";
+
+    protected $fillable = [
+        'id_fabricante',
+        'id_fornecedor',
+        'nome',
+        'categoria',
+        'valor_compra',
+        'valor_venda',
+        'quantidade',
+        'data_emissao',
+        'data_caducidade',
+        'descricao',
+        'estado',
+    ];
+
+    public function fabricante(){
+        return $this->belongsTo(Fabricante::class, 'id_fabricante', 'id');
+    }
+
+    public function fornecedor(){
+        return $this->belongsTo(Fornecedor::class, 'id_fornecedor', 'id');
+    }
 }
