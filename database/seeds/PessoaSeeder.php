be6e565642368bbb491c4f3d1cf548292d8a5596
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-31 21:45:03 +0100

    criando as seeders e trabalhando nelas

---
+++ database/seeds/PessoaSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PessoaSeeder extends Seeder
+{
+    static $pessoas = [
+        'nome'=>"Nicolau NP",
+        'data_nascimento'=>"29-08-1996",
+        'bairro'=>"Forte Santa Rita",
+        'genero'=>"M",
+        'telefone'=>946216795,
+        'email'=>null,
+        'foto'=>null,
+    ];
+
+    public function run()
+    {
+        foreach (Self::$pessoas as $pessoa) {
+            DB::table('pessoas')->insert(
+                $pessoa
+            );
+        }
+    }
+}
