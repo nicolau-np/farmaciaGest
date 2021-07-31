67768daf3f2ee1f5cf7477f92197252c94c841e6
Nicolau NP <mr1Normaliii@gmail.com>
2021-07-31 22:38:40 +0100

    criando metodo para verificar autenticidade

--- app/Http/Controllers/StaticController.php
+++ app/Http/Controllers/StaticController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class StaticController extends Controller
 {
-    //
+    public static function isAuth(){
+        if(Auth::check()){
+            return redirect()->route('home');
+        }
+    }
 }
