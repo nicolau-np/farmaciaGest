b626d120b0239a1d24a70992cbdc6e332fa4f87e
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-01 18:16:30 +0100

    trabalhando no metodo de store

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Funcionario;
+use App\Pessoa;
 use Illuminate\Http\Request;
 
 class FuncionarioController extends Controller
@@ -53,20 +54,45 @@ class FuncionarioController extends Controller
     {
         $request->validate(
             [
-                'nome' => ['required', 'string', 'min:5', 'max:255', 'unique:Funcionarios,nome'],
+                'nome' => ['required', 'string', 'min:5', 'max:255',],
+                'telefone' => ['required', 'integer', 'min:1'],
+                'genero' => ['required', 'string', 'min:1', 'max:255'],
+
+                'cargo'=>['required','string', 'min:1', 'max:255'],
                 'estado' => ['required', 'string', 'min:1', 'max:3'],
             ]
         );
 
-        $data = [
-            'nome'=>$request->nome,
-            'email'=>$request->email,
-            'telefone'=>$request->telefone,
-            'endereco'=>$request->endereco,
-            'estado'=>$request->estado,
+        $data['pessoa'] = [
+            'nome' => $request->nome,
+            'data_nascimento' => $request->data_nascimento,
+            'bairro' => $request->bairro,
+            'genero' => $request->genero,
+            'telefone' => $request->telefone,
+            'email' => $request->email,
+            'foto' => null,
         ];
-        if(Funcionario::create($data)){
-            return back()->with(['success'=>"Feito com sucesso"]);
+        
+        $data['funcionario'] = [
+            'id_pessoa' => null,
+            'cargo' => $request->cargo,
+            'estado' => $request->estado,
+        ];
+
+        if ($request->hasFile('foto') && $request->foto->isValid()) {
+            $request->validate([
+                'foto' => ['required', 'mimes:jpg,jpeg,png,JPG,JPEG,PNG', 'max:5000']
+            ]);
+            $path = $request->file('foto')->store('img_funcionarios');
+            $data['pessoa']['foto'] = $path;
+        }
+
+        $pessoa = Pessoa::create($data['pessoa']);
+        if ($pessoa) {
+            $data['funcionario']['id_pessoa'] = $pessoa->id;
+            if (Funcionario::create($data)) {
+                return back()->with(['success' => "Feito com sucesso"]);
+            }
         }
     }
 
@@ -90,8 +116,8 @@ class FuncionarioController extends Controller
     public function edit($id)
     {
         $funcionario = Funcionario::find($id);
-        if(!$funcionario){
-            return back()->with(['info'=>"Não encontrou"]);
+        if (!$funcionario) {
+            return back()->with(['info' => "Não encontrou"]);
         }
 
         $data = [
@@ -100,10 +126,9 @@ class FuncionarioController extends Controller
             'submenu' => "Editar",
             'type' => "Funcionarios",
             'config' => null,
-            'getFuncionario'=>$funcionario,
+            'getFuncionario' => $funcionario,
         ];
         return view('funcionarios.edit', $data);
-
     }
 
     /**
@@ -116,8 +141,8 @@ class FuncionarioController extends Controller
     public function update(Request $request, $id)
     {
         $funcionario = Funcionario::find($id);
-        if(!$funcionario){
-            return back()->with(['info'=>"Não encontrou"]);
+        if (!$funcionario) {
+            return back()->with(['info' => "Não encontrou"]);
         }
 
         $request->validate(
@@ -127,21 +152,21 @@ class FuncionarioController extends Controller
             ]
         );
 
-        if($request->nome != $funcionario->nome){
+        if ($request->nome != $funcionario->nome) {
             $request->validate([
                 'nome' => ['required', 'string', 'min:5', 'max:255', 'unique:Funcionarios,nome'],
             ]);
         }
 
         $data = [
-            'nome'=>$request->nome,
-            'email'=>$request->email,
-            'telefone'=>$request->telefone,
-            'endereco'=>$request->endereco,
-            'estado'=>$request->estado,
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'telefone' => $request->telefone,
+            'endereco' => $request->endereco,
+            'estado' => $request->estado,
         ];
-        if(Funcionario::find($id)->update($data)){
-            return back()->with(['success'=>"Feito com sucesso"]);
+        if (Funcionario::find($id)->update($data)) {
+            return back()->with(['success' => "Feito com sucesso"]);
         }
     }
 
@@ -154,16 +179,16 @@ class FuncionarioController extends Controller
     public function destroy($id)
     {
         $funcionario = Funcionario::find($id);
-        if(!$funcionario){
-            return back()->with(['info'=>"Não encontrou"]);
+        if (!$funcionario) {
+            return back()->with(['info' => "Não encontrou"]);
         }
 
         $data = [
-            'estado'=>"delete"
+            'estado' => "delete"
         ];
 
-        if(Funcionario::find($id)->update($data)){
-            return back()->with(['info'=>"Eliminado com sucesso"]);
+        if (Funcionario::find($id)->update($data)) {
+            return back()->with(['info' => "Eliminado com sucesso"]);
         }
     }
 }
