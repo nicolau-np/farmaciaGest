7766c2c4d6748e6fe24e18cfdcc025edb134bf71
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 21:20:56 +0100

    criando local para carregamento

--- resources/views/vendas/carrinho.blade.php
+++ resources/views/vendas/carrinho.blade.php
@@ -6,6 +6,7 @@
    <div class="col-md-12">
         <div class="card border-warning">
             <div class="card-body">
+                <div class="form">
                 {{Form::open(['method'=>"post", 'url'=>"", ])}}
                 <div class="row">
                     <div class="col-md-5" style="text-align:center;">
@@ -21,12 +22,18 @@
                         <div class="form-inline">
                             {{Form::text('codigo', null, ['class'=>"form-control", 'placeholder'=>"Codigo de Barra"])}}
                             &nbsp;&nbsp;
-                            <button class="btn btn-primary"><i class="fa fa-sign-in"></i></button>
+                            <button class="btn btn-danger"><i class="fa fa-sign-in"></i></button>
                         </div>
 
                     </div>
                 </div>
                 {{Form::close()}}
+            </div>
+
+            <div class="load_tabela">
+                
+            </div>
+
             </div>
             <div class="card-footer"><small>Last updateed 3 min ago</small>
             </div>
