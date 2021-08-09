6d48d4fcb22d584516274d3a3538f3a3458b9ae3
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 21:08:22 +0100

    update

--- resources/views/vendas/carrinho.blade.php
+++ resources/views/vendas/carrinho.blade.php
@@ -2,9 +2,22 @@
 @section('content')
 
 <div class="row">
+
    <div class="col-md-12">
-       hell
-   </div>
+        <div class="card border-warning">
+            <div class="card-body">
+                <h5 class="card-title">
+
+                </h5>
+                <p class="card-text">
+                    ... ...
+                </p>
+            </div>
+            <div class="card-footer"><small>Last updateed 3 min ago</small>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 @endsection
