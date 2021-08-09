f0059f6f18481063bcb253d9c3139c0e898d4e70
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 19:12:58 +0100

    update list

--- resources/views/vendas/list.blade.php
+++ resources/views/vendas/list.blade.php
@@ -4,16 +4,18 @@
 <div class="row">
 
         <div class="col-lg-3 col-sm-6">
+            <a href="#">
             <div class="card">
                 <div class="card-body">
                     <div class="text-center">
-                        <img src="" class="rounded-circle" alt="">
+                    <img src="{{asset('assets/images/neutro/no-photo-icon-22.png')}}" class="rounded-circle" alt="" style="width:60px; height:60px;">
                         <h5 class="mt-3 mb-1">Ana Liem</h5>
                         <p class="m-0">Senior Manager</p>
                         <!-- <a href="javascript:void()" class="btn btn-sm btn-warning">Send Message</a> -->
                     </div>
                 </div>
             </div>
+            </a>
         </div>
 
 </div>
