3bd8c7608ac2c27b1d00df3d83fdc2cdb68b24f0
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 21:11:24 +0100

    update

--- resources/views/vendas/carrinho.blade.php
+++ resources/views/vendas/carrinho.blade.php
@@ -7,7 +7,11 @@
         <div class="card border-warning">
             <div class="card-body">
                 {{Form::open(['method'=>"post", 'url'=>"", ])}}
-                
+                <div class="row">
+                    <div class="col-md-12" style="text-align:center;">
+                        {{Form::text('produto', null, ['class'=>"form-control", 'placeholder'=>"Nome do produto"])}}
+                    </div>
+                </div>
                 {{Form::close()}}
             </div>
             <div class="card-footer"><small>Last updateed 3 min ago</small>
