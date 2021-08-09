88f7ef69e57f18419a68728324d439d649df8a70
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 19:08:40 +0100

    trabalhando na lista de vendas

--- resources/views/vendas/list.blade.php
+++ resources/views/vendas/list.blade.php
@@ -2,49 +2,20 @@
 @section('content')
 
 <div class="row">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-body">
-                <div class="card-title">
-                    <h4 class="submenu">{{$submenu}}</h4>
-                    <h4 class="link"><a href="/fornecedores/create/" class="btn btn-success btn-sm"><i class="fa fa-add"></i> Novo</a></h4>
-                </div>
 
-                <div class="table-responsive">
-                    @if (session('error'))
-                    <div class="alert bg-danger" style="color:white" role="alert"><em class="fa fa-lg fa-warning">&nbsp;</em> {{__(session('error'))}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
-                    @endif
-
-                    @if (session('success'))
-                    <div class="alert bg-success" style="color:white" role="alert"><em class="fa fa-lg fa-check">&nbsp;</em> {{session('success')}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
-                    @endif
-
-                    @if (session('info'))
-                    <div class="alert bg-info" style="color:white" role="alert"><em class="fa fa-lg fa-check">&nbsp;</em> {{session('info')}} <a href="#" class="pull-right"><em class="fa fa-lg fa-close"></em></a></div>
-                    @endif
-
-                    <table class="table table-striped table-bordered">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Nome</th>
-                                <th>Telefone</th>
-                                <th>Email</th>
-                                <th>Endereço</th>
-                                <th>Estado</th>
-                                <th>Operações</th>
-                            </tr>
-                        </thead>
-
-                    </table>
-
-                    <div class="pagination">
-                        
+        <div class="col-lg-3 col-sm-6">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-center">
+                        <img src="" class="rounded-circle" alt="">
+                        <h5 class="mt-3 mb-1">Ana Liem</h5>
+                        <p class="m-0">Senior Manager</p>
+                        <!-- <a href="javascript:void()" class="btn btn-sm btn-warning">Send Message</a> -->
                     </div>
                 </div>
             </div>
         </div>
-    </div>
+
 </div>
 
 @endsection
