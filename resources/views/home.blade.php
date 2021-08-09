1c7834b07d7fa2fedea03f183bccb37d134d67c9
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-09 09:02:14 +0100

    criando link para venda de produto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,14 @@
     <div class="col-lg-3 col-sm-6">
         <div class="card gradient-1">
             <div class="card-body">
-                <h3 class="card-title text-white">Produtos</h3>
+                <a href="/vendas/create">
+                <h3 class="card-title text-white">Venda</h3>
                 <div class="d-inline-block">
-                    <h2 class="text-white">4565</h2>
-                    <p class="text-white mb-0">Jan - March 2019</p>
+                    <h2 class="text-white">00</h2>
+                    <p class="text-white mb-0">Nova Venda</p>
                 </div>
-                <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
+                <span class="float-right display-5 opacity-5"><i class="fa fa-plus"></i></span>
+                </a>
             </div>
         </div>
     </div>
