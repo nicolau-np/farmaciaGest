ae5a282f03d1b7ae8e78ba3263f6fc1c207756c7
Nicolau NP <mr1Normaliii@gmail.com>
2021-08-17 19:11:08 +0100

    update

--- resources/views/graficos/produtos.blade.php
+++ resources/views/graficos/produtos.blade.php
@@ -19,10 +19,7 @@
                         </p>
                     </figure>
 
-
-
-
-                            <script type="text/javascript">
+                    <script type="text/javascript">
                     Highcharts.chart('container', {
                         chart: {
                             type: 'variablepie'
@@ -40,36 +37,16 @@
                             minPointSize: 10,
                             innerSize: '20%',
                             zMin: 0,
-                            name: 'countries',
-                            data: [{
+                            name: 'produts',
+                            data: [
+
+                            {
                                 name: 'Spain',
                                 y: 505370,
                                 z: 92.9
-                            }, {
-                                name: 'France',
-                                y: 551500,
-                                z: 118.7
-                            }, {
-                                name: 'Poland',
-                                y: 312685,
-                                z: 124.6
-                            }, {
-                                name: 'Czech Republic',
-                                y: 78867,
-                                z: 137.5
-                            }, {
-                                name: 'Italy',
-                                y: 301340,
-                                z: 201.8
-                            }, {
-                                name: 'Switzerland',
-                                y: 41277,
-                                z: 214.5
-                            }, {
-                                name: 'Germany',
-                                y: 357022,
-                                z: 235.6
-                            }]
+                            },
+                            
+                                ]
                         }]
                     });
 
